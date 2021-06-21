150d3d0aa02a307208682f226433978d5cff9f78
Jakub Kolebaba <jakub@kolebaba.cz>
2021-06-21 14:54:04 +0200

    Updated promise typings

--- src/promise.ts
+++ src/promise.ts
@@ -1,25 +1,16 @@
 import { isLeft, Left, Maybe, Right } from './either'
 
 export type UntilResolved = {
-  (promises: []): Promise<Maybe<Error>>;
-  <A>(promises: [Promise<A>]): Promise<Maybe<A>>;
-  <A, B>(promises: [Promise<A>, Promise<B>]): Promise<Maybe<A | B>>;
-  <A, B, C>(promises: [Promise<A>, Promise<B>, Promise<C>]): Promise<Maybe<A | B | C>>;
-  <A, B, C, D>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>]): Promise<Maybe<A | B | C | D>>;
-  <A, B, C, D, E>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>, Promise<E>]): Promise<Maybe<A | B | C | D | E>>;
-  <A, B, C, D, E, F>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>, Promise<E>, Promise<F>]): Promise<Maybe<A | B | C | D | E | F>>;
-  <A, B, C, D, E, F, G>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>, Promise<E>, Promise<F>, Promise<G>]): Promise<Maybe<A | B | C | D | E | F | G>>;
-  <A, B, C, D, E, F, G, H>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>, Promise<E>, Promise<F>, Promise<G>, Promise<H>]): Promise<Maybe<A | B | C | D | E | F | G | H>>;
-  <A, B, C, D, E, F, G, H, I>(promises: [Promise<A>, Promise<B>, Promise<C>, Promise<D>, Promise<E>, Promise<F>, Promise<G>, Promise<H>, Promise<I>]): Promise<Maybe<A | B | C | D | E | F | G | H | I>>;
+  <A>(promises: Promise<A>[]): Promise<Maybe<A>>;
 }
 
-export const untilResolved: UntilResolved = async (arr) => {
+export const untilNotResolved: UntilResolved = async (arr) => {
   const [head, ...rest] = arr;
   if(head) {
     try {
       return Right(await head);
     } catch (e) {
-      return untilResolved(rest)
+      return untilNotResolved(rest)
     }
   }
 
@@ -27,28 +18,18 @@ export const untilResolved: UntilResolved = async (arr) => {
 }
 
 export type UntilRight = {
-  (promises: []): Promise<Maybe<Error>>;
-  <A>(promises: [Promise<Maybe<A>>]): Promise<Maybe<A>>;
-  <A, B>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>]): Promise<Maybe<A | B>>;
-  <A, B, C>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>]): Promise<Maybe<A | B | C>>;
-  <A, B, C, D>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>]): Promise<Maybe<A | B | C | D>>;
-  <A, B, C, D, E>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>, Promise<Maybe<E>>]): Promise<Maybe<A | B | C | D | E>>;
-  <A, B, C, D, E, F>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>, Promise<Maybe<E>>, Promise<Maybe<F>>]): Promise<Maybe<A | B | C | D | E | F>>;
-  <A, B, C, D, E, F, G>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>, Promise<Maybe<E>>, Promise<Maybe<F>>, Promise<Maybe<G>>]): Promise<Maybe<A | B | C | D | E | F | G>>;
-  <A, B, C, D, E, F, G, H>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>, Promise<Maybe<E>>, Promise<Maybe<F>>, Promise<Maybe<G>>, Promise<Maybe<H>>]): Promise<Maybe<A | B | C | D | E | F | G | H>>;
-  <A, B, C, D, E, F, G, H, I>(promises: [Promise<Maybe<A>>, Promise<Maybe<B>>, Promise<Maybe<C>>, Promise<Maybe<D>>, Promise<Maybe<E>>, Promise<Maybe<F>>, Promise<Maybe<G>>, Promise<Maybe<H>>, Promise<Maybe<I>>]): Promise<Maybe<A | B | C | D | E | F | G | H | I>>;
-}
+  <A>(promises: Promise<Maybe<A>>[]): Promise<Maybe<A>>; }
 
-export const untilRight: UntilRight = async (arr) => {
+export const untilNotRight: UntilRight = async (arr) => {
   const [head, ...rest] = arr;
   if(head) {
     const r = await head;
     if(isLeft(r)) {
-      return untilRight(rest);
+      return untilNotRight(rest);
     }
 
     return r;
   }
 
-  return Left(new Error('No promise resolved.'));
+  return Left(new Error('No promise returned right.'));
 }
