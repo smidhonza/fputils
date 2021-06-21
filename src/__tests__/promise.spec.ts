0922dc72c401205d130faf24db04d4a99039c04e
Jakub Kolebaba <jakub@kolebaba.cz>
2021-06-21 14:59:42 +0200

    Fixed tests

--- src/__tests__/promise.spec.ts
+++ src/__tests__/promise.spec.ts
@@ -37,14 +37,14 @@ describe('untilNotRight', () => {
   it('empty array', async () => {
     const res = await untilNotRight([]);
     expect(res.tag).toBe('left');
-    expect(res.value).toEqual(new Error('No promise resolved.'));
+    expect(res.value).toEqual(new Error('No promise returned right.'));
   });
   it('all rejected', async () => {
     const rejection = Promise.resolve(Left(new Error('No.')));
     const res = await untilNotRight([rejection, rejection, rejection]);
 
     expect(res.tag).toBe('left');
-    expect(res.value).toEqual(new Error('No promise resolved.'));
+    expect(res.value).toEqual(new Error('No promise returned right.'));
   });
   it('returns resolved promise', async () => {
     const rejection = Promise.resolve(Left(new Error('No.')));
