ca8cac8ef836a586c2328f0314c4cd517a73334a
Jakub Kolebaba <jakub@kolebaba.cz>
2021-06-21 13:59:11 +0200

    Until right util (#15)

--- src/__tests__/promise.spec.ts
+++ src/__tests__/promise.spec.ts
@@ -1,4 +1,5 @@
-import { untilResolved } from '../promise'
+import { untilResolved, untilRight } from '../promise'
+import { Left, Right } from '../either'
 
 describe('untilResolved', () => {
   it('empty array', async () => {
@@ -26,6 +27,38 @@ describe('untilResolved', () => {
     const rejection = Promise.reject('No.');
 
     const res = await untilResolved([rejection, Promise.resolve('Yes 0.'), rejection, Promise.resolve('Yes 1.')]);
+
+    expect(res.tag).toBe('right');
+    expect(res.value).toEqual('Yes 0.');
+  });
+});
+
+describe('untilRight', () => {
+  it('empty array', async () => {
+    const res = await untilRight([]);
+    expect(res.tag).toBe('left');
+    expect(res.value).toEqual(new Error('No promise resolved.'));
+  });
+  it('all rejected', async () => {
+    const rejection = Promise.resolve(Left(new Error('No.')));
+    const res = await untilRight([rejection, rejection, rejection]);
+
+    expect(res.tag).toBe('left');
+    expect(res.value).toEqual(new Error('No promise resolved.'));
+  });
+  it('returns resolved promise', async () => {
+    const rejection = Promise.resolve(Left(new Error('No.')));
+    const resolution = Promise.resolve(Right('Yes.'));
+
+    const res = await untilRight([rejection, rejection, resolution]);
+
+    expect(res.tag).toBe('right');
+    expect(res.value).toEqual('Yes.');
+  });
+  it('returns first resolved promise', async () => {
+    const rejection = Promise.resolve(Left(new Error('No.')));
+
+    const res = await untilRight([rejection, Promise.resolve(Right('Yes 0.')), rejection, Promise.resolve(Right('Yes 1.'))]);
 
     expect(res.tag).toBe('right');
     expect(res.value).toEqual('Yes 0.');
