663818a832a50a23b9fad835dad64526a09a34fe
Honza Smid <jansmidhonza@gmail.com>
2021-10-01 15:56:47 +0200

    propEq curied even more

--- src/__tests__/propEq.spec.ts
+++ src/__tests__/propEq.spec.ts
@@ -3,7 +3,19 @@ import { propEq } from '../propEq';
 describe("propEq", () => {
   it("returns right property match", () => {
     expect(propEq("name", 5, { name: 5 })).toEqual(true);
+    expect(propEq("name","test 1", { name: "test 1" })).toEqual(true);
+    expect(propEq("name","test 2", { name: "test 1" })).toEqual(false);
+  });
+
+  it("returns right property when match curried", () => {
+    expect(propEq("name", 5)({ name: 5 })).toEqual(true);
     expect(propEq("name", "test 1")({ name: "test 1" })).toEqual(true);
     expect(propEq("name", "test 2")({ name: "test 1" })).toEqual(false);
+  });
+
+  it("returns right property when match curried twice", () => {
+    expect(propEq("name")(5)({ name: 5 })).toEqual(true);
+    expect(propEq("name")("test 1")({ name: "test 1" })).toEqual(true);
+    expect(propEq("name")("test 2")({ name: "test 1" })).toEqual(false);
   });
 });
--- src/propEq.ts
+++ src/propEq.ts
@@ -6,6 +6,7 @@ import { prop } from './prop';
 type PropEq = {
   <T, V>(property: keyof T, value: V, object: T): boolean;
   <T, V>(property: keyof T, value: V): (object: T) => boolean;
+  <T, V>(property: keyof T): (value: V) => (object: T) => boolean;
 }
 
 export const propEq: PropEq = curry(<T, V>(property: keyof T, value: V, object: T) => pipe(object, prop(property), equals(value)));
