2eb7313ddf29ee22b7fc89dca1655560df8ce70b
Honza Smid <jansmidhonza@gmail.com>
2022-08-05 09:42:04 +0200

    refactored

--- src/pipeEither.ts
+++ src/pipeEither.ts
@@ -1,7 +1,9 @@
-import { Right, isLeft, Either, ILeft, IRight } from './either';
-
+import {Right, Either, ILeft, IRight, either, Left} from './either';
+import {head, tail, isEmpty, } from "./array";
+import {pipe} from "./pipe";
 
 type AsyncFn<T, L, R> = (input: T) => Promise<Either<L, R>>;
+const recurse = (functions: any[]) => <T>(res: T) => pipeEither(res, ...functions as [any]);
 
 export async function pipeEither<T, R>(input: T): Promise<IRight<R>>;
 export async function pipeEither<T, L, R>(input: T, f1: AsyncFn<T, L, R>): Promise<Either<L, R>>;
@@ -10,13 +12,11 @@ export async function pipeEither<T, L1, R1, L2, R2, L3, R3>(input: T, f1: AsyncF
 export async function pipeEither<T, L1, R1, L2, R2, L3, R3, L4, R4>(input: T, f1: AsyncFn<T, L1, R1>, f2: AsyncFn<R1, L2, R2>, f3: AsyncFn<R2, L3, R3>, f4: AsyncFn<R3, L4, R4>): Promise<Either<L4, R4> | ILeft<L1> | ILeft<L2> | ILeft<L3>>;
 
 export async function pipeEither(input: any, ...functions: Array<(input: unknown) => Promise<Either<unknown, unknown>>>): Promise<any> {
-  if (functions.length == 0) return Right(input);
-
-  const [head, ...tail] = functions;
-  if (!head) return Right(input);
-
-  const res = await head(input);
-  if (isLeft(res)) return res;
-
-  return pipeEither(res.value, ...(tail as [any]));
+  return isEmpty(functions)
+    ? Right(input)
+    : either(
+      Left,
+      pipe(functions, tail, recurse),
+      await head(functions)(input)
+    )
 }
