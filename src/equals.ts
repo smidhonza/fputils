6d31dda48a7033fd65f193837d9f5ff93749f971
davidtourek <dtourek@centrum.cz>
2022-11-22 14:41:59 +0100

    whitespace to import pipe

--- src/equals.ts
+++ src/equals.ts
@@ -1,6 +1,6 @@
 import { curry } from './curry';
 import { not } from './common';
-import {pipe} from "./pipe";
+import { pipe } from "./pipe";
 
 export type Equals = {
   <A, B>(a: A, b: B): boolean;
